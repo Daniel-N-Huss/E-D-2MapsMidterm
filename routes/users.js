14181c66e2b4c28308994f7e04ec3d8aff6dc72d
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-31 00:46:49 +0000

    set up restful api routes for users

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,12 @@
 
 const express = require('express');
 const router  = express.Router();
-const mapApi  = process.env.google_maps_api;
+
+/* __________________
+    These will all serve responses to ajax requests from the client side
+    which means they need to return promises, likely as json data we can render client side
+*/
+
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -22,6 +27,25 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/:id", (req, res) => {
+    //This will return a user and a list of their maps I think
+    res.send('Hi, nothing created here yet bit your route is good!');
+    const user_id = req.params;
+  });
+
+
+  /*   Routes for possible stretch goals below       */
+
+  // router.post("/:id", (req, res) => {
+    // res.send('Ahoy, this be where we might make requests to edit user profile');
+  // });
+
+  // router.post("/", (req, res) => {
+    // res.send('This would be the route for creating a new user with the login button');
+  // });
+
+
   return router;
 };
 
