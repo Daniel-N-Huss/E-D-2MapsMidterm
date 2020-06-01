55f40c8995e8be8a56fbb02e3e11c918f6dff1c3
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-01 21:55:03 +0000

    slight tweaking to api routes to hand json data to pinPainter / now painting pins to map based on a given map id

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,10 +1,3 @@
-const testPins = [
-  {title: 'Heat', thumbnail_url: 'https://www.master.ca/sn_uploads/fck/heating_history.jpg', description: 'really hot', geo_location: {lat: 34.397, lng: -150.644}, map_id: 1},
-  {title: 'Revolver Coffee', thumbnail_url: 'https://i.imgur.com/Q1pA96P.jpg', description: 'Best coffee in Gastown', geo_location: {lat: 49.283158, lng: -123.109305}, map_id: 2},
-  {title: 'Bakery Brewing', thumbnail_url: 'https://bccraftbeer.com/app/uploads/2019/05/69200421_2357393507689670_5300398940762406912_o.jpg', description: 'Barrel-aged beer brewed in Port Moody B.C.', geo_location: {lat: 49.278991, lng: -122.853245}, map_id: 3}
-  ];
-
-
 document.addEventListener('DOMContentLoaded', function () {
   if (document.querySelectorAll('#map').length > 0)
   {
@@ -17,20 +10,6 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-
-
-const pinPainter = function() {
-
-  testPins.forEach(pin => {
-    marker = new google.maps.Marker({
-      position: pin.geo_location,
-      map: map
-    })
-
-  });
-};
-
-
 var map;
 function initMap() {
   var LHL = {lat: 49.281394, lng: -123.115016};
@@ -40,6 +19,6 @@ function initMap() {
   });
   var marker = new google.maps.Marker({position: LHL, map: map});
 
-  pinPainter();
+  pinPainter(2);
 };
 
--- public/scripts/pinPainter.js
+++ public/scripts/pinPainter.js
@@ -1,9 +1,27 @@
+const testPins = [
+  {title: 'Heat', thumbnail_url: 'https://www.master.ca/sn_uploads/fck/heating_history.jpg', description: 'really hot', geo_location: {lat: 34.397, lng: -150.644}, map_id: 1},
+  {title: 'Revolver Coffee', thumbnail_url: 'https://i.imgur.com/Q1pA96P.jpg', description: 'Best coffee in Gastown', geo_location: {lat: 49.283158, lng: -123.109305}, map_id: 2},
+  {title: 'Bakery Brewing', thumbnail_url: 'https://bccraftbeer.com/app/uploads/2019/05/69200421_2357393507689670_5300398940762406912_o.jpg', description: 'Barrel-aged beer brewed in Port Moody B.C.', geo_location: {lat: 49.278991, lng: -122.853245}, map_id: 3}
+  ];
 
+const pinPainter = function(map_id) {
 
-// async function pinPainter() {
-  // await initMap();
+  getMapPins(map_id)
+    .then(pins => {
 
+      pins.forEach(pin => {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(pin.geo_location.lat, pin.geo_location.lng),
+          map: map
+        })
 
-  // console.log('Hi from an async await!');
+        let infowindow = new google.maps.InfoWindow({
+          content: `<div id="content"> <p>This pin's id: ${pin.id}</p>`
+        })
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        })
+    });
 
-// };
+  });
+};
