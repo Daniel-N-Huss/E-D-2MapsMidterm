a78681f59f5d5b33589aa1b24e9dd572ed393ac4
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-01 23:40:38 +0000

    working on passing variables to initMap function

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 var map;
-function initMap() {
+function initMap(map_id) {
   var LHL = {lat: 49.281394, lng: -123.115016};
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 49.2827, lng: -123.1207},
@@ -19,6 +19,6 @@ function initMap() {
   });
   var marker = new google.maps.Marker({position: LHL, map: map});
 
-  pinPainter(3);
+  pinPainter(map_id);
 };
 
