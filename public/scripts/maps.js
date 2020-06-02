f1aa5cb0f55ac4e5621896ee787307203141c12f
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-02 22:13:25 +0000

    places api now working with pin painting from DB

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -32,12 +32,25 @@
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelectorAll('#map').length > 0)
+  {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src= "https://maps.googleapis.com/maps/api/js?key=&libraries=places&callback=initAutocomplete";
+    script.async = true;
+
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+});
+
 let map;
 let userMarkers = [];
 
 
 window.initAutocomplete = function() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 49.2827, lng: -123.1207},
     zoom: 13,
     mapTypeId: 'roadmap'
