5569dbb10e3c1170a5bb320c9a7ccd453d88e5b4
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-02 21:45:32 +0000

    clicking map cards now removes current pins and paints all the pins matching the cards map id in the database

--- public/scripts/mapLoadListeners.js
+++ public/scripts/mapLoadListeners.js
@@ -2,30 +2,14 @@ const mapCardListener = function() {
   $(`.popular-maps`).click(function() {
     const mapId = this.dataset.mapId;
 
+    const refreshMap = function () {
 
-    const displayMap = function () {
-      const mapOptions = {
-        center: {lat: 49.281394, lng: -123.115016},
-        zoom: 10
-      };
-      const mapDiv = document.getElementById('map');
-      return new Promise (google.maps.Map(mapDiv, mapOptions));
-
-    };
-
-    // console.log('map before new display ' + $("#map").html());
-
-    displayMap().then(() => {console.log('hi');
-    });
-    // const promisedMap = (new Promise(displayMap))
-      // .then(() => {
-        // console.log('map after new display ' + $("#map").html());
-      // })
-      // .catch((err) => {console.log(err);
-      // })
-
-
-
-
+      for (let i = 0; i < userMarkers.length; i++) {
+        userMarkers[i].setMap(null)
+      }
+      userMarkers = []
+      pinPainter(mapId);
+    }
+    refreshMap();
   });
-  }
+}
--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -10,17 +10,21 @@ if (document.querySelectorAll('#map').length > 0)
   }
 }
 
+var LHL = {lat: 49.281394, lng: -123.115016};
+
 var map;
-var marker;
+let userMarkers = [];
+
+
 window.initMap = function() {
 
-  var LHL = {lat: 49.281394, lng: -123.115016};
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 49.2827, lng: -123.1207},
     zoom: 13
   });
   var marker = new google.maps.Marker({position: LHL, map: map});
+  userMarkers.push(marker);
 
 };
 
