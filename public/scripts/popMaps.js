878790eea040c27f57ba0d82a1886559acb25f21
Daniel Zhang <danielzhang95@hotmail.com>
2020-06-04 01:24:16 +0000

    favorites are now shown correspondingly

--- public/scripts/popMaps.js
+++ public/scripts/popMaps.js
@@ -1,4 +1,5 @@
 let isloggedin = false;
+let favourite;
 const renderMaps = function(maps) {
   // append Title
   $('main.maps-container').append(
@@ -7,41 +8,63 @@ const renderMaps = function(maps) {
     `
   );
   // loops through maps
-  for (let i = 0; i < maps.length; i++) {
-    // calls createMapElement for each map
-    const currentMap = createMapElement(maps[i]);
-    // takes return value and appends it to the maps container
-    $('main.maps-container').append(currentMap);
-  }
+  isFavourite(isloggedin).then(()=>{
+    for (let i = 0; i < maps.length; i++) {
+      // calls createMapElement for each map
+
+      const currentMap = createMapElement(maps[i]);
+      // takes return value and appends it to the maps container
+      $('main.maps-container').append(currentMap);
+      favoriteButtonListener(maps[i].id);
+    }
+  })
 };
 
 //Function to create the Element that hosts the map, and to apply
 const createMapElement = function(map) {
-  let $map;
+  let $map; // declare $map variable
 
   if (isloggedin) { // check if user is logged or not
-    console.log("in element");
-
-    $map =
-  // creates map HTML with input from client
-  `<section class="popular-maps" data-map-id="${map.id}">
-    <header>
-      <p class="map-title">${map.title}</p>
-      <p class="owner-id">${map.username}</p>
-    </header>
-    <div class="map-body">
-      <img src="${map.thumbnail_url}" alt="Map Icon"><p class="map-description"> ${map.description}
-      </p>
-    </div>
-    <footer>
-      <p> map created: 7 days ago </p>
-      <span class="material-icons" id="favourite-icon-${map.id}"> favorite_border </span>
-    </footer>
-  </section>`
-  //icon removed: outlined_flag repeat
+    console.log("in element:" + mapIdCheck(favourite, map));
+    if (mapIdCheck(favourite,map)){
+      $map =
+      // creates map HTML that is favourited
+        `<section class="popular-maps" data-map-id="${map.id}">
+        <header>
+          <p class="map-title">${map.title}</p>
+          <p class="owner-id">${map.username}</p>
+        </header>
+        <div class="map-body">
+          <img src="${map.thumbnail_url}" alt="Map Icon"><p class="map-description"> ${map.description}
+          </p>
+        </div>
+        <footer>
+          <p> map created: 7 days ago </p>
+          <span class="material-icons" id="favourite-icon-${map.id}"> favorite </span>
+        </footer>
+      </section>`
+    } else {
+      $map =
+      // creates map HTML that is not favourited
+      `<section class="popular-maps" data-map-id="${map.id}">
+        <header>
+          <p class="map-title">${map.title}</p>
+          <p class="owner-id">${map.username}</p>
+        </header>
+        <div class="map-body">
+          <img src="${map.thumbnail_url}" alt="Map Icon"><p class="map-description"> ${map.description}
+          </p>
+        </div>
+        <footer>
+          <p> map created: 7 days ago </p>
+          <span class="material-icons" id="favourite-icon-${map.id}"> favorite_border </span>
+        </footer>
+      </section>`
+      //icon removed: outlined_flag repeat
+    }
   } else {
     $map =
-  // creates map HTML with input from client
+  // creates map HTML without favourite button
   `<section class="popular-maps" data-map-id="${map.id}">
     <header>
       <p class="map-title">${map.title}</p>
@@ -68,10 +91,7 @@ const loadMaps = () => $.get('/api/maps', JSON)
   isLoggedIn().then(()=>{
     renderMaps(response);
     mapCardListener();
-  }
-
-  )
-
+  })
 });
 
 const isLoggedIn = function() {
@@ -79,7 +99,38 @@ const isLoggedIn = function() {
   .done(function (response) {
     //console.log("response:"+response);
     isloggedin = response;
+    // console.log(isloggedin);
+
   });
+}
+const isFavourite = function(userId, map) {
+  if (isloggedin) {
+    return $.get(`/api/favourites/user/?userId=${userId}`)
+    .done(function (response) {
+      favourite = response;
+    });
+  } else {
+    return $.get(`/api/users/isloggedin`)
+      .done(function (response) {
+
+      });
+  }
+
+}
+const mapIdCheck = function(maps, map) {
+  let bool = false;
+  for (let i of maps){
+    if (i.map_id === map.id){
+      bool = true;
+    }
+  }
+  return bool;
+}
+
+const favoriteButtonListener = function(id) {
+  $(`#favourite-icon-${id}`).click(function(event) {
+    console.log("Favorite button"+id);
+  })
 }
 
 
