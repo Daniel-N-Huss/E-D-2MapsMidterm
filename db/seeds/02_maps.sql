5b531ada31335aa453ff7e0d262d8c9504ff09bf
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-06-05 01:32:01 +0000

    more db seeds

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,6 +1,3 @@
-INSERT INTO maps (title, thumbnail_url, description, owner_id) VALUES ('Best Schools in Vancouver', 'https://d2mvzyuse3lwjc.cloudfront.net/doc/en/UserGuide/images/Heat_Map_Labels/Heatmap_with_Labels-01.png?v=79868', 'A collection of education services to investigate on your road to a career', 1);
+INSERT INTO maps (title, thumbnail_url, description, owner_id) VALUES ('Best Schools in Vancouver', 'https://pbs.twimg.com/profile_images/1197911268939550720/33RhGxsO_400x400.jpg', 'A collection of education services to investigate on your road to a career', 1);
 INSERT INTO maps (title, thumbnail_url, description, owner_id) VALUES ('Neato Coffee Places', 'https://i.imgur.com/Q1pA96P.jpg', 'The most hipster coffee in Vancouver', 2);
-INSERT INTO maps (title, thumbnail_url, description, owner_id) VALUES ('Places Emily Likes To Visit', 'https://static.vinepair.com/wp-content/uploads/2018/06/beer-bubble-cover-mobile.jpg', 'Best breweries in Port Moody', 3);
-
-
-
+INSERT INTO maps (title, thumbnail_url, description, owner_id) VALUES ('Places Emily Likes To Visit', 'https://vancouver.ca/images/cov/feature/vancouver-city-councill-landing.jpg', 'Cool Tourist Spots in Van', 3);
