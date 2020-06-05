49503af3accf36519633c9e074f160112f080dfb
Daniel Zhang <danielzhang95@hotmail.com>
2020-06-05 01:21:59 +0000

    edit and delete implemented

--- public/scripts/myPinDisplay.js
+++ public/scripts/myPinDisplay.js
@@ -12,7 +12,7 @@ const createMyPinElement = function(pin) {
 </div>
   </section>
   <div class="text-center" style="margin-bottom: 16px">
-  <button class="btn btn-primary" id="edit-button-${pin.id}">edit</button>
+  <button class="btn btn-primary" id="edit-button-${pin.id}" onClick="closePinsDisplay(); openNav2()">edit</button>
       <button class="btn btn-primary" id="delete-button-${pin.id}">delete</button>
       <div>
       </div>`
@@ -24,7 +24,8 @@ const renderMyPins = function(pins) {
   for (let i = 0; i < pins.length; i++) {
     const currentPin = createMyPinElement(pins[i]);
     $('#pins-display').append(currentPin);
-    deleteButtonListener(pins[i].id);
+    deleteButtonListener(pins[i]);
+    editButtonListener(pins[i]);
   }
   $('#pins-display').append(
   '<button id="pin-submit" type="submit" class="btn btn-primary pins-button" onClick="closePinsDisplay(); openNav2()"> add more pins!</button>'
@@ -57,23 +58,41 @@ const myPinDisplay = function(mapId) {
 }
 
 
-const deleteButtonListener = function(pin_id) {
-  console.log("delete", pin_id);
-
+const deleteButtonListener = function(pin) {
+  // console.log("delete", pin_id);
+  const pin_id = pin.id;
   $(`#delete-button-${pin_id}`).click(function(event) {
     console.log("del ",pin_id);
 
 
 
-    //   $.post(`/api/favourites/user/add/?user_id=${user_id}&map_id=${map_id}`)
-    //   .done(function (response) {
-    //   console.log(response);
-    //   $(`#favourite-icon-${map_id}`).replaceWith(`<span class="material-icons" id="favourite-icon-${map.id}"> favorite </span>`)
-    // })
 
-    // isFavourite(isloggedin).then(()=>{
-    //   favoriteButtonListener(user_id, map);
-    // })
+    $.post(`/api/pins/delete/${pin_id}`)
+    .done(function (response) {
+      console.log(response);
+      $('#pins-display').empty();
+      myPinDisplay(pin.map_id);
+    })
+
+
+  })
+}
+const editButtonListener = function(pin) {
+  // console.log("delete", pin_id);
+  const pin_id = pin.id;
+  $(`#edit-button-${pin_id}`).click(function(event) {
+    console.log("del ",pin_id);
+
+
+
+
+    $.post(`/api/pins/delete/${pin_id}`)
+    .done(function (response) {
+      console.log(response);
+      $('#pins-display').empty();
+      myPinDisplay(pin.map_id);
+    })
+
 
   })
 }
